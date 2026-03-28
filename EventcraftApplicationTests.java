66bcf97f9d3150ed2f391d1c31661b8326a69b01
Sanket santosh chor <157352443+Sanketchor@users.noreply.github.com>
2026-03-28 12:41:39 +0530

    Add files via upload

---
+++ EventcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Eventcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
