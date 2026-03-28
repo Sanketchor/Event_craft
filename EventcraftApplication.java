b46d05c329f68bb7971d63d76aa488b5aebc3f04
Sanket santosh chor <157352443+Sanketchor@users.noreply.github.com>
2026-03-28 12:40:30 +0530

    Add files via upload

---
+++ EventcraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.Eventcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventcraftApplication.class, args);
+	}
+
+}
